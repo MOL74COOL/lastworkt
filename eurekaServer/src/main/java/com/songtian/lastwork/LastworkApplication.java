ae3761faccf447b7e3fa64ce69ac6cc4a3ddd286
china-coolman <2924166627@qq.com>
2020-12-12 20:55:06 +0800

    first

---
+++ eurekaServer/src/main/java/com/songtian/lastwork/LastworkApplication.java
@@ -0,0 +1,13 @@
+package com.songtian.lastwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastworkApplication.class, args);
+    }
+
+}
---
+++ eurekaServer/src/test/java/com/songtian/lastwork/LastworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songtian.lastwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
